3cdcc88b3a189ef4840fd305678e530eb5180b82
Nshuti Jabes <nshutij7@gmail.com>
2022-07-21 22:29:01 +0200

    worked on the issues on the loader section.

--- js/script.js
+++ js/script.js
@@ -20,3 +20,13 @@ window.onscroll = () =>{
   }
 
 }
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+  
+  function fadeOut(){
+    setInterval(loader, 3000);
+  }
+  
+  window.onload = fadeOut();
